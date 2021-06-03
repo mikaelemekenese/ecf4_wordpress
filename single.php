ee3e21a897c0fba907385991618353519c633599
Mikaele Mekenese <m.mekenese@cci-formation.nc>
2021-06-03 21:54:31 +1100

    new commit

--- single.php
+++ single.php
@@ -9,24 +9,28 @@ get_header(); ?>
     <div class="col-8">
 
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+          
           <article class="post">
 
-            <h1><?php the_title(); ?></h1><br>
-          
-            <?php the_post_thumbnail(); ?><br><br>
+              <h1><?php the_title(); ?></h1><br>
+            
+              <?php the_post_thumbnail(); ?><br><br>
 
-            <div class="post__meta">
-              <?php echo get_avatar(get_the_author_meta('ID'), 40); ?>
-              <p>
-                Publié le <?php the_date(); ?><br>
-                Par <?php the_author(); ?><br>
-                Dans la catégorie : <?php the_category(); ?><br>
-              </p>
-            </div>
+              <div class="post__meta">
+                  
+                  <span><?php echo get_avatar(get_the_author_meta('ID'), 40); ?></span>
+                  <span>
+                    <p>
+                      Publié le <?php the_date(); ?><br>
+                      Par <?php the_author(); ?><br>
+                      <?php the_category(); ?>
+                  </p>
+                </span>
+              </div>
 
-            <div class="post__content">
-              <?php the_content(); ?>
-            </div>
+              <div class="post__content">
+                <?php the_content(); ?>
+              </div>
 
           </article>
 
