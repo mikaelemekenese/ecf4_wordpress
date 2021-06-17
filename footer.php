e22913d006cc03249daa577fc16d83fa2f0790a0
Mikaele Mekenese <m.mekenese@cci-formation.nc>
2021-06-17 21:13:36 +1100

    new commit

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
-        <footer class="site__footer">
-            <!-- <?php wp_nav_menu(array('theme_location' => 'footer')); ?> -->
+        <footer class="footer">
+            <p>Tous droits réservés  |  Mikaele MEKENESE  ©2021</p>
         </footer>
 
         <?php wp_footer(); ?>
-        </body>
-
-        </html>
+    </body>
+</html>
